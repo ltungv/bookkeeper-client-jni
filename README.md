4634cc8e4c3f7cf655eebdc2c6b1e56d15fd5460
ltungv <15053897+ltungv@users.noreply.github.com>
2024-08-10 14:50:36 +0200

    impl: use jni-bind

--- README.md
+++ README.md
@@ -9,3 +9,11 @@ Do the following steps to get it working (only Linux is supported):
 4. Run `cmake -DJAVA_HOME=<path to JAVA_HOME> ..` and `make` to build the executable
 5. Change directory to the project's root (classpath is hardcoded)
 5. Run the executable `./build/bookkeeper-client-jni`
+
+# References
+
++ JNI specification:
+    + https://docs.oracle.com/en/java/javase/17/docs/specs/jni/
++ Managing object life-cycle:
+    + https://stackoverflow.com/questions/9098890/how-to-check-for-memory-leaks-in-jni
+    + https://stackoverflow.com/questions/42310107/what-are-the-rules-for-jni-local-references-returned-to-a-program-that-embeds-a
--- src/main.cpp
+++ src/main.cpp
@@ -1,130 +1,128 @@
+#include "jni.h"
+#include "jni_bind_release.h"
+
 #include <cstring>
+#include <exception>
 #include <filesystem>
 #include <iostream>
-#include <jni.h>
 #include <stdio.h>
 #include <string>
 
 static const jint JNI_VERSION = JNI_VERSION_10;
 
-namespace java {
+static constexpr jni::Class jclassThrowable{
+    "java/lang/Throwable",
+    jni::Method{"getMessage", jni::Return{jstring{}}},
+};
 
-namespace lang {
+static constexpr jni::Class jclassDigestType{
+    "org/apache/bookkeeper/client/BookKeeper$DigestType",
+    jni::Static{
+        jni::Field{"DUMMY", jni::Self{}},
+    },
+};
 
-static jclass jc_Throwable;
-static jmethodID jm_Throwable_getMessage;
+static constexpr jni::Class jclassAbstractConfiguration{
+    "org/apache/bookkeeper/conf/AbstractConfiguration",
+};
 
-} // namespace lang
+static constexpr jni::Class jclassClientConfiguration{
+    "org/apache/bookkeeper/conf/ClientConfiguration",
+    jni::Constructor{},
+    jni::Method{"setMetadataServiceUri", jni::Return{jclassAbstractConfiguration}, jni::Params{jstring{}}},
+};
 
-} // namespace java
+static constexpr jni::Class jclassLedgerHandle{
+    "org/apache/bookkeeper/client/LedgerHandle",
+    jni::Method{"append", jni::Return{jlong{}}, jni::Params{jni::Array{jbyte{}}}},
+    jni::Method{"close", jni::Return{}},
+};
 
-namespace bookkeeper {
+static constexpr jni::Class jclassBookKeeper{
+    "org/apache/bookkeeper/client/BookKeeper",
+    jni::Constructor{jclassClientConfiguration},
+    jni::Method{"createLedger", jni::Return{jclassLedgerHandle}, jni::Params{jint{}, jint{}, jclassDigestType, jni::Array{jbyte{}}}},
+    jni::Method{"close", jni::Return{}},
+};
 
-namespace conf {
-
-static jclass jc_ClientConfiguration;
-static jmethodID jm_ClientConfiguration_init;
-static jmethodID jm_ClientConfiguration_setMetadataServiceUri;
-
-} // namespace conf
-
-namespace client {
-
-static jclass jc_BookKeeper;
-static jmethodID jm_BookKeeper_init;
-static jmethodID jm_BookKeeper_createLedger;
-static jmethodID jm_BookKeeper_close;
-
-static jclass jc_BookKeeper_DigestType;
-static jfieldID jf_BookKeeper_DigestType_DUMMY;
-
-static jclass jc_LedgerHandle;
-static jmethodID jm_LedgerHandle_append;
-static jmethodID jm_LedgerHandle_close;
-
-} // namespace client
-
-} // namespace bookkeeper
-
-jclass mustFindClass(JNIEnv *env, const std::string &name) {
-  jclass clazz = env->FindClass(name.c_str());
-  if (clazz == nullptr) {
-    env->ExceptionDescribe();
+void throw_jvm_exception(JNIEnv *env) {
+  jobject jvm_throwable = env->ExceptionOccurred();
+  if (jvm_throwable != nullptr) {
     env->ExceptionClear();
-    exit(1);
-  }
-  return clazz;
-}
-
-jfieldID mustGetStaticFieldID(JNIEnv *env, const jclass clazz, const std::string &name, const std::string &signature) {
-  jfieldID id = env->GetStaticFieldID(clazz, name.c_str(), signature.c_str());
-  if (id == nullptr) {
-    env->ExceptionDescribe();
-    env->ExceptionClear();
-    exit(1);
-  }
-  return id;
-}
-
-jmethodID mustGetMethodID(JNIEnv *env, const jclass clazz, const std::string &name, const std::string &signature) {
-  jmethodID id = env->GetMethodID(clazz, name.c_str(), signature.c_str());
-  if (id == nullptr) {
-    env->ExceptionDescribe();
-    env->ExceptionClear();
-    exit(1);
-  }
-  return id;
-}
-
-void loadClassAndMethodReferences(JNIEnv *env) {
-  // Load and cache java.lang.Throwable.
-  {
-    jclass clazz = mustFindClass(env, "java/lang/Throwable");
-    java::lang::jc_Throwable = (jclass)env->NewGlobalRef(clazz);
-    java::lang::jm_Throwable_getMessage = mustGetMethodID(env, clazz, "getMessage", "()Ljava/lang/String;");
-    env->DeleteLocalRef(clazz);
-  }
-  // Load and cache org.apache.bookkeeper.conf.ClientConfiguration.
-  {
-    jclass clazz = mustFindClass(env, "org/apache/bookkeeper/conf/ClientConfiguration");
-    bookkeeper::conf::jc_ClientConfiguration = (jclass)env->NewGlobalRef(clazz);
-    bookkeeper::conf::jm_ClientConfiguration_init = mustGetMethodID(env, clazz, "<init>", "()V");
-    bookkeeper::conf::jm_ClientConfiguration_setMetadataServiceUri = mustGetMethodID(env, clazz, "setMetadataServiceUri", "(Ljava/lang/String;)Lorg/apache/bookkeeper/conf/AbstractConfiguration;");
-    env->DeleteLocalRef(clazz);
-  }
-  // Load and cache org.apache.bookkeeper.client.BookKeeper.
-  {
-    jclass clazz = mustFindClass(env, "org/apache/bookkeeper/client/BookKeeper");
-    bookkeeper::client::jc_BookKeeper = (jclass)env->NewGlobalRef(clazz);
-    bookkeeper::client::jm_BookKeeper_init = mustGetMethodID(env, clazz, "<init>", "(Lorg/apache/bookkeeper/conf/ClientConfiguration;)V");
-    bookkeeper::client::jm_BookKeeper_createLedger = mustGetMethodID(env, clazz, "createLedger", "(IILorg/apache/bookkeeper/client/BookKeeper$DigestType;[B)Lorg/apache/bookkeeper/client/LedgerHandle;");
-    bookkeeper::client::jm_BookKeeper_close = mustGetMethodID(env, clazz, "close", "()V");
-    env->DeleteLocalRef(clazz);
-  }
-  // Load and cache org.apache.bookkeeper.client.BookKeeper.DigestType.
-  {
-    jclass clazz = mustFindClass(env, "org/apache/bookkeeper/client/BookKeeper$DigestType");
-    bookkeeper::client::jc_BookKeeper_DigestType = (jclass)env->NewGlobalRef(clazz);
-    bookkeeper::client::jf_BookKeeper_DigestType_DUMMY = mustGetStaticFieldID(env, clazz, "DUMMY", "Lorg/apache/bookkeeper/client/BookKeeper$DigestType;");
-    env->DeleteLocalRef(clazz);
-  }
-  // Load and cache org.apache.bookkeeper.client.LedgerHandle.
-  {
-    jclass clazz = mustFindClass(env, "org/apache/bookkeeper/client/LedgerHandle");
-    bookkeeper::client::jc_LedgerHandle = (jclass)env->NewGlobalRef(clazz);
-    bookkeeper::client::jm_LedgerHandle_append = mustGetMethodID(env, clazz, "append", "([B)J");
-    bookkeeper::client::jm_LedgerHandle_close = mustGetMethodID(env, clazz, "close", "()V");
-    env->DeleteLocalRef(clazz);
+    jni::LocalObject<jclassThrowable> throwable{jvm_throwable};
+    jni::LocalString message = throwable("getMessage");
+    jni::UtfStringView message_pinned = message.Pin();
+    std::string_view message_view = message_pinned.ToString();
+    throw std::runtime_error(message_view.data());
   }
 }
 
-void unloadClassAndMethodReferences(JNIEnv *env) {
-  env->DeleteGlobalRef(java::lang::jc_Throwable);
-  env->DeleteGlobalRef(bookkeeper::conf::jc_ClientConfiguration);
-  env->DeleteGlobalRef(bookkeeper::client::jc_BookKeeper);
-  env->DeleteGlobalRef(bookkeeper::client::jc_BookKeeper_DigestType);
-  env->DeleteGlobalRef(bookkeeper::client::jc_LedgerHandle);
-}
+class LedgerHandle {
+private:
+  jni::GlobalObject<jclassLedgerHandle> instance;
+
+public:
+  LedgerHandle(jni::GlobalObject<jclassLedgerHandle> &instance) : instance{std::move(instance)} {}
+
+  ~LedgerHandle() {
+    try {
+      close();
+    } catch (std::exception &e) {
+      std::cerr << "Ledger close error: " << e.what() << std::endl;
+    }
+  }
+
+  jlong append(const char *const payload_content, int payload_length) {
+    jni::LocalArray<jbyte> payload{payload_length};
+    {
+      jni::ArrayView<jbyte> pinned = payload.Pin();
+      memcpy(pinned.ptr(), payload_content, payload_length);
+    }
+    jlong entry_id = instance("append", payload);
+    throw_jvm_exception(jni::JniEnv::GetEnv());
+    return entry_id;
+  }
+
+  void close() {
+    instance("close");
+    throw_jvm_exception(jni::JniEnv::GetEnv());
+  }
+};
+
+class BookKeeper {
+private:
+  jni::GlobalObject<jclassBookKeeper> instance;
+
+public:
+  BookKeeper(jni::GlobalObject<jclassClientConfiguration> &client_configuration)
+      : instance{jni::GlobalObject<jclassBookKeeper>{client_configuration}} {
+    throw_jvm_exception(jni::JniEnv::GetEnv());
+  }
+
+  ~BookKeeper() {
+    try {
+      close();
+    } catch (std::exception &e) {
+      std::cerr << "BookKeeper close error: " << e.what() << std::endl;
+    }
+  }
+
+  LedgerHandle createLedger(int ensemble_size, int quorum_size, jni::GlobalObject<jclassDigestType> &digest_type, const char *const digest_password_content, int digest_password_length) {
+    jni::LocalArray<jbyte> digest_password{digest_password_length};
+    {
+      jni::ArrayView<jbyte> pinned = digest_password.Pin();
+      memcpy(pinned.ptr(), digest_password_content, digest_password_length);
+    }
+    jni::GlobalObject<jclassLedgerHandle> ledger_handle = instance("createLedger", jint{ensemble_size}, jint{quorum_size}, digest_type, digest_password);
+    throw_jvm_exception(jni::JniEnv::GetEnv());
+    return LedgerHandle(ledger_handle);
+  }
+
+  void close() {
+    instance("close");
+    throw_jvm_exception(jni::JniEnv::GetEnv());
+  }
+};
 
 std::string readJarPaths(std::string directory) {
   std::string jar_paths = "";
@@ -145,83 +143,53 @@ std::string readJarPaths(std::string directory) {
   return jar_paths;
 }
 
-void run(JNIEnv *env) {
-  jobject obj_bookkeeper_client_config = env->NewObject(bookkeeper::conf::jc_ClientConfiguration, bookkeeper::conf::jm_ClientConfiguration_init);
-  jstring zookeeper_address = env->NewStringUTF("zk+hierarchical://localhost:2181/ledgers");
-  env->CallNonvirtualObjectMethod(obj_bookkeeper_client_config, bookkeeper::conf::jc_ClientConfiguration, bookkeeper::conf::jm_ClientConfiguration_setMetadataServiceUri, zookeeper_address);
-  if (env->ExceptionCheck()) {
-    env->ExceptionDescribe();
-    env->ExceptionClear();
-    return;
-  }
-  jobject obj_bookkeeper_client = env->NewObject(bookkeeper::client::jc_BookKeeper, bookkeeper::client::jm_BookKeeper_init, obj_bookkeeper_client_config);
-  if (env->ExceptionCheck()) {
-    env->ExceptionDescribe();
-    env->ExceptionClear();
-    return;
-  }
-  jobject digest_type = env->GetStaticObjectField(bookkeeper::client::jc_BookKeeper_DigestType, bookkeeper::client::jf_BookKeeper_DigestType_DUMMY);
-  jbyteArray digest_password = env->NewByteArray(0);
-  jobject obj_ledger_handle = env->CallNonvirtualObjectMethod(obj_bookkeeper_client, bookkeeper::client::jc_BookKeeper, bookkeeper::client::jm_BookKeeper_createLedger, 3, 3, digest_type, digest_password);
-  if (!env->ExceptionCheck()) {
-    std::string payload_content = "hello";
-    for (int i = 0; i < 100; i++) {
-      jbyteArray payload = env->NewByteArray(5);
-      jbyte *payload_buffer = env->GetByteArrayElements(payload, nullptr);
-      memcpy(payload_buffer, payload_content.c_str(), 5);
-      env->ReleaseByteArrayElements(payload, payload_buffer, JNI_ABORT);
-      jlong entry_id = env->CallNonvirtualLongMethod(obj_ledger_handle, bookkeeper::client::jc_LedgerHandle, bookkeeper::client::jm_LedgerHandle_append, payload);
-      if (env->ExceptionCheck()) {
-        env->ExceptionDescribe();
-        env->ExceptionClear();
-      } else {
-        printf("Append entry with id=%ld\n", entry_id);
-      }
-      env->DeleteLocalRef(payload);
+void run(JavaVM *jvm) {
+  jni::JvmRef<jni::kDefaultJvm> jvm_ref = jni::JvmRef<jni::kDefaultJvm>(jvm);
+
+  jni::GlobalObject<jclassClientConfiguration> client_configuration{};
+  client_configuration("setMetadataServiceUri", "zk+hierarchical://localhost:2181/ledgers");
+
+  BookKeeper bookkeeper_client = BookKeeper(client_configuration);
+
+  jni::LocalArray<jbyte> digest_password{0};
+  jni::GlobalObject<jclassDigestType> digest_type = jni::StaticRef<jclassDigestType>{}["DUMMY"].Get();
+  LedgerHandle ledger_handle = bookkeeper_client.createLedger(3, 3, digest_type, "", 0);
+
+  std::string payload_content = "hello";
+  for (int i = 0; i < 100; i++) {
+    try {
+      jlong entry_id = ledger_handle.append(payload_content.c_str(), payload_content.length());
+      std::cout << "Appended id=" << entry_id << std::endl;
+    } catch (std::exception &e) {
+      std::cerr << "Append error: " << e.what() << std::endl;
     }
-    env->CallNonvirtualVoidMethod(obj_ledger_handle, bookkeeper::client::jc_LedgerHandle, bookkeeper::client::jm_LedgerHandle_close);
-    if (env->ExceptionCheck()) {
-      env->ExceptionDescribe();
-      env->ExceptionClear();
-    }
-  } else {
-    env->ExceptionDescribe();
-    env->ExceptionClear();
-  }
-  env->CallNonvirtualVoidMethod(obj_ledger_handle, bookkeeper::client::jc_LedgerHandle, bookkeeper::client::jm_LedgerHandle_close);
-  if (env->ExceptionCheck()) {
-    env->ExceptionDescribe();
-    env->ExceptionClear();
-  }
-  env->CallNonvirtualVoidMethod(obj_bookkeeper_client, bookkeeper::client::jc_BookKeeper, bookkeeper::client::jm_BookKeeper_close);
-  if (env->ExceptionCheck()) {
-    env->ExceptionDescribe();
-    env->ExceptionClear();
   }
 }
 
 int main(int argc, char **argv) {
   (void)argc;
   (void)argv;
+
   std::string classpath = "-Djava.class.path=";
   classpath.append(readJarPaths("vendor"));
+
   JavaVMOption options[1];
   options[0].optionString = classpath.data();
+
   JavaVMInitArgs init_args = {
       .version = JNI_VERSION,
       .nOptions = 1,
       .options = options,
       .ignoreUnrecognized = false,
   };
+
   JavaVM *jvm;
   JNIEnv *env;
   if (JNI_CreateJavaVM(&jvm, (void **)&env, (void *)&init_args) != JNI_OK) {
     fprintf(stderr, "Failed to create the JVM\n");
     return 1;
   }
-  loadClassAndMethodReferences(env);
-  run(env);
-  unloadClassAndMethodReferences(env);
-  jvm->DestroyJavaVM();
+
+  run(jvm);
   return 0;
 }
